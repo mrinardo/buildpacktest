4ed16ec26ef735bafd8eed4e99093269d0464d47
Mauricio Rinardo <mauricio.rinardo@gmail.com>
2022-02-19 17:33:55 -0300

    Test

--- azure-appservice-container/deploy.groovy
+++ azure-appservice-container/deploy.groovy
@@ -1,14 +1,15 @@
 try {
-    // Updates webapp with new Docker image via AZURE-CLI
-    azureSetWebAppContainer steps: steps,
-        resourceGroup: env.AZURE_RESOURCE_GROUP,
-        webappName:    env.AZURE_WEBAPP_NAME,
-        imageWithTag: env.IMAGE_NAME,
-        containerRegistryURL: env.IMAGE_REGISTRY,
-        containerRegistryUser: env.AZURE_CLIENT_ID, //DEPS_CREDENTIAL_USR,
-        containerRegistryPwd: env.AZURE_CLIENT_SECRET //DEPS_CREDENTIAL_PSW,
-        deploymentSlot: env.AZURE_DEPLOYMENT_SLOT ?: ""
-    
+    withCredentials([usernamePassword(credentialsId: 'MySP', passwordVariable: 'AZURE_CLIENT_SECRET', usernameVariable: 'AZURE_CLIENT_ID')]) {
+        // Updates webapp with new Docker image via AZURE-CLI
+        azureSetWebAppContainer steps: steps,
+            resourceGroup: env.AZURE_RESOURCE_GROUP,
+            webappName:    env.AZURE_WEBAPP_NAME,
+            imageWithTag: env.IMAGE_NAME,
+            containerRegistryURL: env.IMAGE_REGISTRY,
+            containerRegistryUser: env.AZURE_CLIENT_ID, //DEPS_CREDENTIAL_USR,
+            containerRegistryPwd: env.AZURE_CLIENT_SECRET //DEPS_CREDENTIAL_PSW,
+            deploymentSlot: env.AZURE_DEPLOYMENT_SLOT ?: ""
+    }
 } catch(Exception error){
     error("Error while updating WebApp container on Azure environment: $error")
 }
